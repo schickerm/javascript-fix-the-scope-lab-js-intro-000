d8874a1d476e05d2958f336897a06ad1496a9737
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-10 19:09:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = funkyFunction() {
-  return function() 
+  return function() {
    return "FUNKY!"
+  }
+}
